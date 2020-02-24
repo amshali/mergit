f559bf1d87b9b9ae46c2f5cb944237855b8e1eaa
Amin Shali <amshali@gmail.com>
2020-02-23 23:43:31 -0800

    progress

--- mergit.js
+++ mergit.js
@@ -7,8 +7,21 @@ var argv = require('optimist')
     .string('merged')
     .demand(['local', 'remote', 'merged'])
     .argv;
+var express = require('express');
+var app = require('express')();
+var http = require('http').Server(app);
+
+app.use('/diff-match-patch', express.static(__dirname + '/node_modules/diff-match-patch'));
+app.use('/codemirror', express.static(__dirname + '/node_modules/codemirror-minified'));
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
 
 console.log('Local: ' + argv.local);
 console.log('Remote: ' + argv.remote);
 console.log('Merged: ' + argv.merged);
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(argv.port, '0.0.0.0');
 
